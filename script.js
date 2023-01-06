8059bd6b3f63c8189b4047f21716d3ff066dbe86
mohammad laliyani <mohammad3camaro@yahoo.com>
2023-01-06 18:23:33 +0330

    v1

---
+++ script.js
@@ -0,0 +1,54 @@
+const preview = document.querySelector("#preview");
+const previewScale = document.querySelector("#preview-scale");
+const previewFlip = document.querySelector("#preview-flip");
+const brightnessSlider = document.querySelector("#brightness");
+const brightnessSliderValue = document.querySelector("#brightness-value");
+const rotateSlider = document.querySelector("#rotate");
+const rotateSliderValue = document.querySelector("#rotate-value");
+
+const handleRotate = () => {
+  const rotate = rotateSlider.value;
+  rotateSliderValue.innerText = rotate;
+
+  // TODO: write your code here
+};
+
+const handleBrightness = () => {
+  const brightness = brightnessSlider.value;
+  brightnessSliderValue.innerText = brightness;
+
+  // TODO: write your code here
+};
+
+const handleFilter = (e) => {
+  const { target } = e;
+  const { id: filter } = target;
+  // filter: "grayscale" | "sepia" | "invert" | "hue-rotate" | "contrast" | "saturate" | "blur"
+
+  // TODO: write your code here
+};
+
+const handleFlip = (flip) => {
+  //  flip: "vertical" | "horizontal"
+
+  // TODO: write your code here
+};
+
+const handleMouseEnter = () => {
+  // TODO: write your code here
+};
+
+const handleMouseLeave = () => {
+  // TODO: write your code here
+};
+
+const handleMouseMove = (e) => {
+  const imageWidth = previewScale.offsetWidth;
+  const imageHeight = previewScale.offsetHeight;
+  const imageOffsetTop = previewScale.offsetTop;
+  const imageOffsetLeft = previewScale.offsetLeft;
+  const pageX = e.pageX;
+  const pageY = e.pageY;
+
+  // TODO: write your code here
+};
